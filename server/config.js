cede95fd3568a2d345845f54e0f71e5ad5af7398
Mike Adams <michaelglenadams@gmail.com>
2013-04-07 08:38:25 -0700

    remove accounts ui

--- server/config.js
+++ server/config.js
@@ -1,36 +1,36 @@
-Accounts.loginServiceConfiguration.remove({
-    service: "facebook"
-})
+// Accounts.loginServiceConfiguration.remove({
+//     service: "facebook"
+// })
 
-Accounts.loginServiceConfiguration.insert({
-    service: "facebook",
-    clientid: "483074125063030",
-    secret: "e0ac540687c8c593529db8710f34104f"
-})
+// Accounts.loginServiceConfiguration.insert({
+//     service: "facebook",
+//     clientid: "483074125063030",
+//     secret: "e0ac540687c8c593529db8710f34104f"
+// })
 
-Accounts.onCreateUser(function(options, user){
-    console.log(user)
-    var accessToken = user.services.facebook.accessToken,
-    result,
-    profile;
+// Accounts.onCreateUser(function(options, user){
+//     console.log(user)
+//     var accessToken = user.services.facebook.accessToken,
+//     result,
+//     profile;
 
-    result = Meteor.http.get("http://graph.facebook.com/me/Picture", {
-        params: {
-            access_token: accessToken
-        }
-    });
+//     result = Meteor.http.get("http://graph.facebook.com/me/Picture", {
+//         params: {
+//             access_token: accessToken
+//         }
+//     });
 
-    if(result.error)
-        throw result.error
+//     if(result.error)
+//         throw result.error
 
-    profile = _.pick(result.data,
-        "id",
-        "name",
-        "link",
-        "username",
-        "gender");
+//     profile = _.pick(result.data,
+//         "id",
+//         "name",
+//         "link",
+//         "username",
+//         "gender");
 
-    user.profile = profile;
+//     user.profile = profile;
 
-    return user;
-})
+//     return user;
+// })
