39e5ebbc1da9e46deab31ac81dea0fef96c05d89
John Katsnelson <voltalocke@gmail.com>
2013-04-06 21:31:44 -0700

    removed hello template

--- client/client.js
+++ client/client.js
@@ -96,9 +96,3 @@ Meteor.isClient && (window.test = function () {
     }, i * 10);
   });
 });
-
-Template.hello.events = {
-  'click .btn' : function() {
-    getImage('http://www.thejunglestore.com/core/media/media.nl?id=37516&c=432681&h=3c579cf84403f4536d5b');
-  }
-}
