1e77fa5ec555809e987895e71c1f4a28f95f1fa9
adnan <mail@adnanwahab.com>
2013-04-06 19:56:57 -0700

    test url

--- mosaic.js
+++ mosaic.js
@@ -18,54 +18,56 @@ if (Meteor.isServer) {
   });
 }
 
-
-
-var rgb = getAverageRGB(document.getElementById('i'));
-document.body.style.backgroundColor = 'rgb('+rgb.r+','+rgb.b+','+rgb.g+')';
-
-
 function getAverageRGB(selector) {
   var canvas = $('canvas')[0],
       context = canvas.getContext && canvas.getContext('2d');
 
   if (! canvas) throw new Error('this function needs a canvas el');
 
-    var blockSize = 5, 
-        defaultRGB = [0, 0, 0],
-        data, width, height,
-        i = -4,
-        length,
-        rgb = [0, 0, 0],
-        count = 0, 
-        imgEl = 'string' === typeof selector ? document.querySelector(selector) : selector;
-        
-    if (!context) return defaultRGB;
-    
-    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+  var blockSize = 5, 
+      defaultRGB = [0, 0, 0],
+      data, width, height,
+      i = -4,
+      length,
+      rgb = [0, 0, 0],
+      count = 0, 
+      imgEl = 'string' === typeof selector ? document.querySelector(selector) : selector;
+  
+  if (!context) return defaultRGB;
+  
+  height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
 
-    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-    
-    context.drawImage(imgEl, 0, 0);
-    
-    try {
-        data = context.getImageData(0, 0, width, height);
-    } catch(e) {
-      alert('x');
-      return defaultRGB;
-    }
-    
-    length = data.data.length;
-    
-    while ( (i += blockSize * 4) < length ) {
-        ++count;
-        rgb[0] += data.data[i];
-        rgb[1] += data.data[i+1];
-        rgb[2] += data.data[i+2];
-    }
-    
-    rgb[0] = ~~ (rgb[0] / count);
-    rgb[1] = ~~ (rgb[1] / count);
-    rgb[2] = ~~ (rgb[2] / count);
-    
-    return rgb;
+  width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+  
+  context.drawImage(imgEl, 0, 0);
+  
+  try {
+    data = context.getImageData(0, 0, width, height);
+  } catch(e) {
+    alert('x');
+    return defaultRGB;
+  }
+  
+  length = data.data.length;
+  
+  while ( (i += blockSize * 4) < length ) {
+    ++count;
+    rgb[0] += data.data[i];
+    rgb[1] += data.data[i+1];
+    rgb[2] += data.data[i+2];
+  }
+  
+  rgb[0] = ~~ (rgb[0] / count);
+  rgb[1] = ~~ (rgb[1] / count);
+  rgb[2] = ~~ (rgb[2] / count);
+  
+  return rgb;
 }
+
+Meteor.isClient && (window.test = function () {
+  window.test_url.forEach(function (url, i) {
+    setTimeout(function (){
+      $('<img>').attr('src', url).appendTo('body');
+    }, i * 10);
+  });
+});
