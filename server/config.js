e7ef4906c0f16d3b6c44f2b5486e28a83f68fa10
Mike Adams <michaelglenadams@gmail.com>
2013-04-07 08:54:43 -0700

    fixes facebook account stuff

--- server/config.js
+++ server/config.js
@@ -8,29 +8,28 @@
 //     secret: "e0ac540687c8c593529db8710f34104f"
 // })
 
-// Accounts.onCreateUser(function(options, user){
-//     console.log(user)
-//     var accessToken = user.services.facebook.accessToken,
-//     result,
-//     profile;
+Accounts.onCreateUser(function(options, user){
+    console.log(user)
+    var accessToken = user.services.facebook.accessToken;
+    Session.set('accessToken', accessToken)
 
-//     result = Meteor.http.get("http://graph.facebook.com/me/Picture", {
-//         params: {
-//             access_token: accessToken
-//         }
-//     });
+    // result = Meteor.http.get("http://graph.facebook.com/me/Picture", {
+    //     params: {
+    //         access_token: accessToken
+    //     }
+    // });
 
-//     if(result.error)
-//         throw result.error
+    // if(result.error)
+    //     throw result.error
 
-//     profile = _.pick(result.data,
-//         "id",
-//         "name",
-//         "link",
-//         "username",
-//         "gender");
+    // profile = _.pick(result.data,
+    //     "id",
+    //     "name",
+    //     "link",
+    //     "username",
+    //     "gender");
 
-//     user.profile = profile;
+    // user.profile = profile;
 
-//     return user;
-// })
+    // return user;
+})
